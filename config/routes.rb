9c46d5705972b20932c6ff7533ae1dc2784a9c2a
Ford1508 <phuclc1508@gmail.com>
2023-03-28 00:34:30 +0700

    login-signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/home', to: 'static_pages#home'
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create show)
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  resources :account_activations, only: :edit
+  resources :password_resets, only: %i(new create edit update) 
+  resources :account_activations, only: :edit
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
