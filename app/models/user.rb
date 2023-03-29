0be3fad8b9dc77356368836a7c56d287865f82fd
Ford1508 <phuclc1508@gmail.com>
2023-03-29 23:54:05 +0700

    activate account

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     
     before_save :downcase_email
+    before_create :create_activation_digest
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
@@ -58,6 +59,10 @@ class User < ApplicationRecord
 
     def downcase_email
         self.email.downcase!
+    end
+    def create_activation_digest
+        self.activation_token = User.new_token
+        self.activation_digest = User.digest(activation_token)
     end
 
 end
