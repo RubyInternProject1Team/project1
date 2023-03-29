22c3d457023db8a8e7f28c6d84fa556efb28b730
HaiPhong <haiphong.huynh@gmail.com>
2023-03-29 20:01:39 +0700

    item chua xong

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+  include ProductsHelper
+  before_action :load_filter, only: :home
+
   def home
+    load_products
   end
 end
---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ApplicationRecord
+    belongs_to :category
+    has_many :picture_details, dependent: :destroy
+    has_many :order_items, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :rates, dependent: :destroy
+    has_many :orders, through: :order_items
+    has_many :user_rates, through: :rates, source: :user
+    has_many :user_comments, through: :comments, source: :user
+
+    scope :get_category_id, ->(category_id){where(category_id: category_id) if category_id.first.present?}
+end
